c683b4fa905a0d7431364a64f52cdbb5f72b6213
MeeWeeL <MeeWeeL@yandex.ru>
2021-10-25 21:01:46 +0300

    CustomView

--- app/src/main/java/com/meeweel/materialdesign/util/EquilateralImageView.kt
+++ app/src/main/java/com/meeweel/materialdesign/util/EquilateralImageView.kt
@@ -2,6 +2,7 @@ package geekbarains.material.util
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 
 class EquilateralImageView @JvmOverloads constructor(
@@ -12,5 +13,7 @@ class EquilateralImageView @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        val scale: ScaleType = ScaleType.CENTER_CROP
+        scaleType = scale // Установил дефолтный scaleType
     }
 }
